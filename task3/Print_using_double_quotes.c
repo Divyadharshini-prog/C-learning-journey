d2e1a27d4d3de4f0940b32d2aecac1a0bea8b1d8
Divyadharshini-prog <dharshiniperiyar277@gmail.com>
2024-12-19 15:41:31 +0530

    Updated some comments in Print_using_double_quotes.c

--- task3/Print_using_double_quotes.c
+++ task3/Print_using_double_quotes.c
@@ -5,14 +5,29 @@ int main() {
     char dept[100]; 
     int rollno;     
     printf("Enter name: ");
-    scanf("%s", &name); 
+    scanf("%s", &name);        
     printf("Enter department: ");
     scanf("%s", &dept); 
     printf("Enter roll number: ");
     scanf("%d", &rollno);
 
-    printf("\"NAME \" : \"%s\"\n", name);
+    printf("\"NAME \" : \"%s\"\n", name);   // to print the output with double quotes use \" name \"
     printf("\"DEPT\" : \" %s\"\n", dept);
-    printf("\"ROLLNO\" : \"%d\n", rollno);
+    printf("\"ROLLNO\" : \"%d\"n", rollno);
     return 0;
 }
+
+
+/* To run this code use cmd: gcc Print_using_double_quotes.c -o Print_using_double_quotes.exe
+cmd : gcc Print_using_double_quotes.c -o Print_using_double_quotes.exe
+cmd : .\Print_using_double_quotes.exe 
+*/
+/* sample Input:
+Enter name: Divya
+Enter department: ECE
+Enter roll number: 2021007
+Output:
+"NAME " : "Divya"
+"DEPT" : " ECE"
+"ROLLNO" : "2021007"
+*/
