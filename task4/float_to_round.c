2f3571c9d06cafaf5924cc698d50938b22c81185
Divyadharshini-prog <dharshiniperiyar277@gmail.com>
2024-12-19 17:00:07 +0530

    Updated the code with math.h and round function

--- task4/float_to_round.c
+++ task4/float_to_round.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-
+#include <math.h>
 int main() {
     float num;
     printf("Enter a number : ",num);
     scanf("%f",&num);
-    float rounded = (float)((int)(num * 10 + 0.5)) / 10; // Manual rounding
-    printf("%.1f\n", rounded); // Outputs 67.1
+    printf("%f", round(num));
+    //float rounded = (float)((int)(num * 10 + 0.5)) / 10; // Manual rounding
+    //printf("%.1f\n", rounded); 
     return 0;
 }
+
