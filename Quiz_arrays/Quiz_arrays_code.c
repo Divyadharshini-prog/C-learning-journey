8f37bfe23f4ac19492d49ca062fd61ccf17b900b
Divyadharshini-prog <dharshiniperiyar277@gmail.com>
2025-01-25 18:39:01 +0530

    Updated comments in Quiz_arrays_code.c

--- Quiz_arrays/Quiz_arrays_code.c
+++ Quiz_arrays/Quiz_arrays_code.c
@@ -53,5 +53,20 @@ output:
 204
 99
 16
+The actual error in this code is not in the loop but conversion of unsigned to signed data type
+correct code:
+  #include<stdio.h>
+  #define TOTAL_ELEMENTS (sizeof(array) / sizeof(array[0]))
+  int array[] = {23,34,12,17,204,99,16};
+
+  int main()
+  {
+      int d;
+
+      for(d=-1;d <= (int)(TOTAL_ELEMENTS-2);d++)
+          printf("%d\n",array[d+1]);
+
+      return 0;
+  }
 
 */
