e7a92ecf369fd8f39a49ff8ecb7d1b1656639561
Divya <dharshiniperiyar277@gmail.com>
2025-04-03 11:41:58 +0530

    Updated Sum_and_difference_two_numbers.c

--- arithmetic_operation/Sum_and_difference_two_numbers.c
+++ arithmetic_operation/Sum_and_difference_two_numbers.c
@@ -5,16 +5,16 @@ int main()
 {
     int num1,num2,sum,sub;
     float n1,n2,plus,minus;
-    printf("");
+    printf("Enter two integers: ");
     scanf("%d %d", &num1, &num2);
-    printf("");
+    printf("Enter two floating-point numbers: ");
     scanf("%f %f", &n1 ,&n2);
     sum = num1 +num2 ;
     sub = num1 - num2;
     plus = n1 + n2;
     minus = n1 - n2;
-    printf("%d %d",sum,sub);
-	printf("\n%.1f %.1f",plus,minus);
+    printf(" %d  %d",sum,sub);
+    printf("\n%.1f %.1f",plus,minus);
     
     return 0;
 }
